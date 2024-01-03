3156e8d888800a80beca679c6a6bd45e3f4497fd
seygodin <111480823+seygodin@users.noreply.github.com>
2024-01-03 09:57:22 +0900

    Add files via upload

---
+++ utils/get_scrn_size.py
@@ -0,0 +1,15 @@
+import pyautogui
+import macmouse as mouse
+import time
+
+print(pyautogui.size()[0])
+
+screen_width, screen_height = pyautogui.size()
+
+print(screen_height, screen_width)
+
+
+pyautogui.alert("hahah")
+
+#while True:
+#    print(pyautogui.position())
